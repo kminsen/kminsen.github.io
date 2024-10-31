f4056ea652b9df6456337592c86e92911d1102f6
Koay Min Sen <mskoay1999@hotmail.com>
2024-10-31 14:33:26 +0800

    completed v1 cyan theme
    Needs to be pushed and deploy on github pages

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const hLinks = document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
+});
+
+hLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
 });
